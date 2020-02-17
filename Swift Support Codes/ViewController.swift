301aceb613384dc186f66480123f3c94ecff1062
Чингиз Куандык <k.chyngiz@gmail.com>
2020-02-17 11:06:07 +0600

    Initial Commit

---
+++ Swift Support Codes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Support Codes
+//
+//  Created by Shyngys Kuandyk on 2/17/20.
+//  Copyright © 2020 Shyngys Kuandyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
